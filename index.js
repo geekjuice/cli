6ee97a3dc49b7d18ad851da0c91b8cabec0c1543
Nicholas Hwang <hello@undefined.engineer>
2019-05-17 13:20:12 -0400

    [squash]

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 #!/usr/bin/env node
 
+const React = require('react');
+const { render, Color } = require('ink');
 const main = require('./src/main');
+
+class Counter extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      i: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({
+        i: this.state.i + 1,
+      });
+    }, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    return <Color green>{this.state.i} tests passed</Color>;
+  }
+}
 
 (async () => {
   try {
-    await main();
+    render(<Counter />);
+    // await main();
   } catch (error) {
     require('./src/error')(error);
     process.exit(1);
